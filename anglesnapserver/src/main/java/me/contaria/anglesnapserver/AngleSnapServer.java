56f39caa43ffcc41276b0d635abf57f8690c548f
klumzie <36917461+klumzie@users.noreply.github.com>
2025-07-23 07:48:25 +0100

    Update AngleSnapServer.java

--- anglesnapserver/src/main/java/me/contaria/anglesnapserver/AngleSnapServer.java
+++ anglesnapserver/src/main/java/me/contaria/anglesnapserver/AngleSnapServer.java
@@ -1,10 +1,22 @@
 package me.contaria.anglesnapserver;
 
 import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AngleSnapServer implements ModInitializer {
+
+    public static final String MOD_ID = "anglesnapserver";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
     @Override
     public void onInitialize() {
-        new ArrowTracker(); // Initialize arrow tracking
+        LOGGER.info("Initializing AngleSnapServer...");
+
+        // Register arrow tracker for player join/disconnect
+        ArrowTracker tracker = new ArrowTracker();
+        tracker.register();
+
+        LOGGER.info("AngleSnapServer loaded!");
     }
 }
